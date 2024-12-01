71563c1dcf288f31cc05a317b3a3e2f657cb12da
Mahjabin Monika <mmonika@3-D-6.42heilbronn.de>
2024-12-01 18:25:59 +0100

    Norme

--- Makefile
+++ Makefile
@@ -5,37 +5,31 @@ PRINTF_ARCHIVE = $(PRINTF_PATH)/libftprintf.a
 
 C_SRC =	client.c utils.c
 S_SRC =	server.c
+
+C_OBJ = $(C_SRC:.c=.o)
+S_OBJ = $(S_SRC:.c=.o)
+
 C_NAME = client
 S_NAME = server
 
-CB_SRC = client_bonus.c utils.c
-SB_SRC = server_bonus.c
-CB_NAME = client_bonus
-SB_NAME = server_bonus
-
 all: $(C_NAME) $(S_NAME)
-bonus:	$(CB_NAME) $(SB_NAME)
+bonus:	all
 
-$(C_NAME): $(C_SRC) $(PRINTF_ARCHIVE)
-	$(CC) $(CFLAGS) -o $(C_NAME) $(C_SRC) $(PRINTF_ARCHIVE)
+$(C_NAME): $(C_OBJ) $(PRINTF_ARCHIVE)
+	$(CC) $(CFLAGS) -o $(C_NAME) $(C_OBJ) $(PRINTF_ARCHIVE)
 
-$(S_NAME): $(S_SRC) $(PRINTF_ARCHIVE)
-	$(CC) $(CFLAGS) -o $(S_NAME) $(S_SRC) $(PRINTF_ARCHIVE)
-
-$(CB_NAME): $(CB_SRC) $(PRINTF_ARCHIVE)
-	$(CC) $(CFLAGS) -o $(CB_NAME) $(CB_SRC) $(PRINTF_ARCHIVE)
-
-$(SB_NAME): $(SB_SRC) $(PRINTF_ARCHIVE)
-	$(CC) $(CFLAGS) -o $(SB_NAME) $(SB_SRC) $(PRINTF_ARCHIVE)
+$(S_NAME): $(S_OBJ) $(PRINTF_ARCHIVE)
+	$(CC) $(CFLAGS) -o $(S_NAME) $(S_OBJ) $(PRINTF_ARCHIVE)
 
 $(PRINTF_ARCHIVE):
-	$(MAKE) -C $(PRINTF_PATH)
+	$(MAKE) -C $(PRINTF_PATH) all
 
 clean:
 	$(MAKE) -C $(PRINTF_PATH) clean
+	rm -f $(C_OBJ) $(S_OBJ)
 
 fclean: clean
 	$(MAKE) -C $(PRINTF_PATH) fclean
-	rm -f $(C_NAME) $(S_NAME) $(CB_NAME) $(SB_NAME)
+	rm -f $(C_NAME) $(S_NAME)
 
 re: fclean all
--- client.c
+++ client.c
@@ -5,19 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmonika <mmonika@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/06 19:21:59 by mmonika           #+#    #+#             */
-/*   Updated: 2024/12/01 17:22:08 by mmonika          ###   ########.fr       */
+/*   Created: 2024/11/10 17:43:09 by mmonika           #+#    #+#             */
+/*   Updated: 2024/12/01 18:20:17 by mmonika          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
+static void	sigcon_handler(int mysignal)
+{
+	if (mysignal == SIGUSR1)
+		ft_printf("\e[1;32mThank you for your message 😊\e[0m\n");
+	exit(0);
+}
+
 int	main(int argc, char *argv[])
 {
-	int		server_pid;
+	struct sigaction	sca;
+	int					server_pid;
 
+	sca.sa_handler = sigcon_handler;
+	sigemptyset(&sca.sa_mask);
+	sca.sa_flags = 0;
+	sigaction(SIGUSR1, &sca, NULL);
 	if (argc == 3 && argv[2][0] != '\0')
 	{
+		signal(SIGUSR1, sigcon_handler);
 		server_pid = ft_atoi(argv[1]);
 		if (check_pid(server_pid) == 1)
 			send_bit(server_pid, argv[2]);
@@ -30,22 +43,3 @@ int	main(int argc, char *argv[])
 	}
 	return (0);
 }
-
-/*
-SIGUSR1 send 1;
-SIGUSR2 send 0;
-
-h in binary 0110 1000 ;
-(h >> 7) & 1 ; (0 1101000 >> 7) & 1 ; (0 & 1) ; 0 ; send SIGUSR2
-(h >> 6) & 1 ; (01 101000 >> 6) & 1 ; (1 & 1) ; 1 ; send SIGUSR1
-(h >> 5) & 1 ; (011 01000 >> 5) & 1 ; (1 & 1) ; 1 ; send SIGUSR1
-(h >> 4) & 1 ; (0110 1000 >> 4) & 1 ; (0 & 1) ; 0 ; send SIGUSR2
-(h >> 3) & 1 ; (01101 000 >> 3) & 1 ; (1 & 1) ; 1 ; send SIGUSR1
-(h >> 2) & 1 ; (011010 00 >> 2) & 1 ; (0 & 1) ; 0 ; send SIGUSR2
-(h >> 1) & 1 ; (0110100 0 >> 1) & 1 ; (0 & 1) ; 0 ; send SIGUSR2
-(h >> 0) & 1 ; (01101000  >> 0) & 1 ; (0 & 1) ; 0 ; send SIGUSR2
-e in binary 0110 0101 ; (e >> 7) & 1;
-l in binary 0110 1100
-l in binary 0110 1100
-o in binary 0110 1111
-*/
